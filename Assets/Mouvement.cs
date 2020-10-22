ee6b99581e98bcd9f524a3bbe2f11641357d4499
JuliaClem <juliaclement23@gmail.com>
2020-10-22 15:27:37 +0200

    Animations
    Idle + Run

--- Assets/Mouvement.cs
+++ Assets/Mouvement.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class Mouvement : MonoBehaviour
 {
     //Attributs
-    public float Speed = 5.0f;
+    public float Speed = 3.0f;
+    public float turnSpeed = 15f;
+    float Xangle = 90f;
+    float Yangle = 90f;
+    float Zangle = 90f;
+    private Animator anim;
+    private Rigidbody rb;
 
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
      void Update() 
     {
         float axisZ = Input.GetAxis("Vertical");
-        float axisX = Input.GetAxis("Horizontal");
+        float axisX = Input.GetAxis("Horizontal"); 
         
         // Marche avant
         if (axisZ > 0)
@@ -28,13 +39,24 @@ public class Mouvement : MonoBehaviour
         if (axisX > 0)
         {
             transform.Translate(transform.right * Speed * axisX * Time.deltaTime);
-            Debug.Log("Touche D detectée.");
+            Debug.Log("Touche D detectée.");  
         }
         //Gauche
         if (axisX < 0)
         {
             transform.Translate(transform.right * Speed * axisX * Time.deltaTime);
             Debug.Log("Touche Q detectée.");
+        }
+
+        //Animation Idle to Run
+
+        if (axisZ != 0 || axisX != 0)
+        {
+            anim.SetFloat("Speed", 1);
+        }
+        else 
+        {
+            anim.SetFloat("Speed", 0);
         }
     }
 }
