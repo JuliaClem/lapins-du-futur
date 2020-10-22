70368269cbec85cd3cc8d9afde5b28641b03d7c1
JuliaClem <juliaclement23@gmail.com>
2020-10-22 23:40:24 +0200

    Last Updates

--- Assets/Mouvement.cs
+++ Assets/Mouvement.cs
@@ -7,12 +7,23 @@ public class Mouvement : MonoBehaviour
     //Attributs
     public float Speed = 3.0f;
     public float turnSpeed = 15f;
-    float Xangle = 90f;
-    float Yangle = 90f;
-    float Zangle = 90f;
     private Animator anim;
     private Rigidbody rb;
+    AudioSource Jump;
+    [SerializeField] AudioClip sndJump;
 
+    public void PlaySoundJump()
+        {
+            Jump.volume = 0.15f;
+            Jump.pitch = 1f;
+            Jump.PlayOneShot(sndJump);
+        }
+
+    private void Awake()
+    {
+        Jump = GetComponent<AudioSource>();
+    }
+    
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -21,31 +32,35 @@ public class Mouvement : MonoBehaviour
      void Update() 
     {
         float axisZ = Input.GetAxis("Vertical");
-        float axisX = Input.GetAxis("Horizontal"); 
+        float axisX = Input.GetAxis("Horizontal");
         
         // Marche avant
         if (axisZ > 0)
         {
             transform.Translate(transform.forward * Speed * axisZ * Time.deltaTime);
             Debug.Log("Touche Z detectée.");
+            Jump.PlayOneShot(sndJump);
         }
         // Marche arrière
         if (axisZ < 0)
         {
             transform.Translate(transform.forward * Speed * axisZ * Time.deltaTime);
             Debug.Log("Touche S detectée.");
+            Jump.PlayOneShot(sndJump);
         }
         // Droite
         if (axisX > 0)
         {
             transform.Translate(transform.right * Speed * axisX * Time.deltaTime);
-            Debug.Log("Touche D detectée.");  
+            Debug.Log("Touche D detectée.");
+            Jump.PlayOneShot(sndJump);  
         }
         //Gauche
         if (axisX < 0)
         {
             transform.Translate(transform.right * Speed * axisX * Time.deltaTime);
             Debug.Log("Touche Q detectée.");
+            Jump.PlayOneShot(sndJump);
         }
 
         //Animation Idle to Run
